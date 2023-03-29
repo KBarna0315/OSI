a1f38610a31d2b1f39fc67c5b4065166c7fdb644
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:26:08 +0200

    dev: Basic transport layer functions created

--- models/Transport_layer.php
+++ models/Transport_layer.php
@@ -4,5 +4,17 @@ namespace models;
 
 class Transport_layer
 {
+    public function establishConnection($destination) { //Establish a connection with the destination node, if using a connection-oriented protocol like TCP.
+
+    }
+    public function terminateConnection() { // Terminate the connection with the destination node.
+
+    }
+    public function sendData($data, $destination){
+
+    }
+    public function receiveData() { //Receive data from the source node and handle retransmissions, if necessary.
+
+    }
 
 }
