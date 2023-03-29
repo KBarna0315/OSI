620e957ce44f8b9dd674ec890d63b508845bd7a9
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:31:07 +0200

    dev: Basic presentation layer functions created

--- models/Presentation_layer.php
+++ models/Presentation_layer.php
@@ -4,5 +4,17 @@ namespace models;
 
 class Presentation_layer
 {
+    public function formatData($data) { // Convert data to a common format that both sender and receiver can understand.
+
+    }
+    public function unformatData($data) { //Convert received data to the original format.
+
+    }
+    public function encryptData($data) { //Encrypt data for secure transmission.
+
+    }
+    public function decryptData($data) {  //Decrypt received data.
+
+    }
 
 }
