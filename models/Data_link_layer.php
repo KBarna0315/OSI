d6a7735fafa05629e6dd2547e939b7f75f596116
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:22:35 +0200

    dev: Basic data_link layer functions created

--- models/Data_link_layer.php
+++ models/Data_link_layer.php
@@ -4,5 +4,11 @@ namespace models;
 
 class Data_link_layer
 {
+    public function encodeFrames($data) { //Encapsulate data into frames and add necessary headers, such as source and destination MAC addresses.
+
+    }
+    public function decodeFrames($frames) { //Extract data from received frames and validate the source and destination MAC addresses.
+
+    }
 
 }
