11132c9baa6c76d193209bea150b48cef52a6cf7
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:09:50 +0200

    dev: Some comments

--- controllers/osi_simulation_controller.php
+++ controllers/osi_simulation_controller.php
@@ -1,1 +1,2 @@
 <?php
+//This class will interact with the OSI layer classes (models) and manage the simulation.
--- views/osi_simulation_view.php
+++ views/osi_simulation_view.php
@@ -1,1 +1,2 @@
 <?php
+//This file will be responsible for displaying the simulation's results to the user. It can be as simple as displaying the original data, transmitted data, and received data.
