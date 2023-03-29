ba8429f9b50d7cbdbaf283d1939f3f42f8a5881b
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:24:23 +0200

    dev: Basic network layer functions created

--- models/Network_layer.php
+++ models/Network_layer.php
@@ -4,5 +4,14 @@ namespace models;
 
 class Network_layer
 {
+    public function routePacket($data,$destination) { //Determine the best route for data transmission based on the destination IP address.
+
+    }
+    public function forwardPacket($packet) { //Forward the packet to the next hop in the routing path.
+
+    }
+    public function handleIncomingPacket($packet) { //Process received packets and deliver them to the appropriate application.
+
+    }
 
 }
