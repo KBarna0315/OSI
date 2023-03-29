e2a1687e960edfc15b4d567c1f069861f485ed3a
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:27:13 +0200

    dev: Basic session layer functions created

--- models/Session_layer.php
+++ models/Session_layer.php
@@ -4,5 +4,14 @@ namespace models;
 
 class Session_layer
 {
+    public function createSession($destination) { //Establish a session with the destination node.
+
+    }
+    public function closeSession() { //Terminate the session with the destination node.
+
+    }
+    public function handleSessionErrors() { //Handle session-related errors and interruptions.
+
+    }
 
 }
