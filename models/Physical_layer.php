236c8940e50d22bfffd841a914046110715fdc9b
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:20:28 +0200

    dev: Basic physical layer functions created

--- models/Physical_layer.php
+++ models/Physical_layer.php
@@ -4,5 +4,11 @@ namespace models;
 
 class Physical_layer
 {
+    public function transmitBits($bits) { //Convert input data into electrical signals for transmission.
+
+    }
+    public function reciveBits($bits) { // Convert received electrical signals into digital data.
+
+    }
 
 }
