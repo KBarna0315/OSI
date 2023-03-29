26eb40021dbf6d6b1a10b5d9634bef20a8ce4cb3
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:20:45 +0200

    dev: typo

--- models/Physical_layer.php
+++ models/Physical_layer.php
@@ -7,7 +7,7 @@ class Physical_layer
     public function transmitBits($bits) { //Convert input data into electrical signals for transmission.
 
     }
-    public function reciveBits($bits) { // Convert received electrical signals into digital data.
+    public function receiveBits($bits) { // Convert received electrical signals into digital data.
 
     }
 
