09911648d764173fcc9ebe427ae8a49d9ef3dbc6
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:42:29 +0200

    dev: Frontend progress

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+require_once 'controllers/osi_simulation_controller.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    header('Content-Type: application/json');
+    $input = json_decode(file_get_contents('php://input'), true);
+    $data = $input['data'];
+
+    $controller = new OsiSimulationController();
+    $transmittedData = $controller->simulateDataTransmission($data);
+    $receivedData = $controller->simulateDataReception($transmittedData);
+
+    $result = [
+        'originalData' => $data,
+        'transmittedData' => $transmittedData,
+        'receivedData' => $receivedData,
+    ];
+
+    echo json_encode($result);
+} else {
+    // If it's not a POST request, display the frontend HTML
+    require_once 'index.html';
+}
