531410498d1fe7819f36a7ecb0561f9e27f25910
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 01:33:43 +0200

    dev: Basic application layer functions created

--- models/Application_layer.php
+++ models/Application_layer.php
@@ -4,5 +4,14 @@ namespace models;
 
 class Application_layer
 {
+    public function sendRequest($request) { //Send an application-level request to the server, such as an HTTP
+
+    }
+    public function receiveRequest() { // Process incoming application-level requests and generate
+
+    }
+    public function sendResponse($response) { //Send a response to the client based on the received request.
+
+    }
 
 }
