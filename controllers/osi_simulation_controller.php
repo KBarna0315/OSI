21b051d345a81e22e64e4782141ec27da274689f
KBarna0315 <kskovacsb@gmail.com>
2023-03-30 02:18:12 +0200

    dev: Controller progress

--- controllers/osi_simulation_controller.php
+++ controllers/osi_simulation_controller.php
@@ -1,2 +1,65 @@
 <?php
-//This class will interact with the OSI layer classes (models) and manage the simulation.
+
+use models\Physical_layer;
+use models\Data_link_layer;
+use models\Network_layer;
+use models\Transport_layer;
+use models\Session_layer;
+use models\Presentation_layer;
+use models\Application_layer;
+
+require_once 'models/Physical_layer.php';
+require_once 'models/Data_link_layer.php';
+require_once 'models/Network_layer.php';
+require_once 'models/Transport_layer.php';
+require_once 'models/Session_layer.php';
+require_once 'models/Presentation_layer.php';
+require_once 'models/Application_layer.php';
+
+
+class OsiSimulationController {
+    private $physicalLayer;
+    private $dataLinkLayer;
+    private $networkLayer;
+    private $transportLayer;
+    private $sessionLayer;
+    private $presentationLayer;
+    private $applicationLayer;
+
+    public function __construct() {
+        // Instantiate OSI layer classes
+        $this->physicalLayer = new Physical_layer();
+        $this->dataLinkLayer = new Data_link_layer();
+        $this->networkLayer = new Network_layer();
+        $this->transportLayer = new Transport_layer();
+        $this->sessionLayer = new Session_layer();
+        $this->presentationLayer = new Presentation_layer();
+        $this->applicationLayer = new Application_layer();
+    }
+
+    public function simulateDataTransmission($data) {
+        // Process data through the OSI layers (top to bottom)
+/*        $data = $this->applicationLayer->sendRequest($data);
+        $data = $this->presentationLayer->formatData($data);
+        $data = $this->sessionLayer->createSession($data);
+        $data = $this->transportLayer->sendData($data);
+        $data = $this->networkLayer->routePacket($data);
+        $data = $this->dataLinkLayer->encodeFrames($data);
+        $data = $this->physicalLayer->transmitBits($data);
+
+        return $data;*/
+    }
+
+    public function simulateDataReception($receivedData) {
+        // Process received data through the OSI layers (bottom to top)
+/*        $data = $this->physicalLayer->receiveBits($receivedData);
+        $data = $this->dataLinkLayer->decodeFrames($data);
+        $data = $this->networkLayer->handleIncomingPacket($data);
+        $data = $this->transportLayer->receiveData($data);
+        $data = $this->sessionLayer->closeSession($data);
+        $data = $this->presentationLayer->unformatData($data);
+        $data = $this->applicationLayer->receiveRequest($data);
+
+        return $data;*/
+    }
+}
