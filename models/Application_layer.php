44421c07b3e44c3ed03110d2bbc4cc4ee3af7b3d
KBarna0315 <kskovacsb@gmail.com>
2023-03-31 00:30:47 +0200

    dev: Data Packet creation

--- models/Application_layer.php
+++ models/Application_layer.php
@@ -4,14 +4,40 @@ namespace models;
 
 class Application_layer
 {
+    private $osiSimulationController;
+    public function __construct($osiSimulationController) {
+        $this->osiSimulationController = $osiSimulationController;
+    }
     public function sendRequest($request) { //Send an application-level request to the server, such as an HTTP
+        // Perform any application-specific request processing (e.g., formatting)
 
+        // Convert the request into a data packet with appropriate headers, similar to a TCP segment
+        $dataPacket = $this->createDataPacket($request);
+
+        // Pass the data packet to the OsiSimulationController for transmission through the OSI layers
+        $transmittedData = $this->osiSimulationController->simulateDataTransmission($dataPacket);
+
+        // Process the transmitted data (e.g., display a success message or handle errors)
+        // ...
     }
     public function receiveRequest() { // Process incoming application-level requests and generate
 
     }
     public function sendResponse($response) { //Send a response to the client based on the received request.
 
+    }
+    public function createDataPacket($request) {
+        // Create a data packet with appropriate headers, similar to a TCP segment
+        // For simplicity, we'll represent the packet as an associative array
+        $dataPacket = [
+            'source_port' => 12345, // Example source port
+            'destination_port' => 80, // Example destination port
+            'sequence_number' => 0, // Example sequence number
+            'acknowledgment_number' => 0, // Example acknowledgment number
+            'data' => $request // The application-level request
+        ];
+
+        return $dataPacket;
     }
 
 }
