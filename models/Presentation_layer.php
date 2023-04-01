70a49c1d88e9122624a8a463c78232e1aec8c066
KBarna0315 <kskovacsb@gmail.com>
2023-04-01 03:03:52 +0200

    dev: Presentation_layer Decrypt and unformat the data

--- models/Presentation_layer.php
+++ models/Presentation_layer.php
@@ -4,7 +4,7 @@ namespace models;
 
 class Presentation_layer
 {
-
+    private $encryptionKey; //Store key to use the same in the formatData and unformatData
 
 
 
@@ -24,21 +24,41 @@ class Presentation_layer
      * @return string The encrypted and formatted data
      */
     public function formatData($data) {
-        $key = $this->generateEncryptionKey();
+        $this->encryptionKey = $this->generateEncryptionKey();
         $cipher = 'aes-256-cbc'; // Choose the encryption algorithm
         $ivlen = openssl_cipher_iv_length($cipher);
         $iv = openssl_random_pseudo_bytes($ivlen); // Generate an initialization vector
         // Encrypt the data using the key and the chosen cipher
-        $encryptedData = openssl_encrypt($data, $cipher, $key, 0, $iv);
+        $encryptedData = openssl_encrypt($data, $cipher, $this->encryptionKey, 0, $iv);
         // Combine the initialization vector and the encrypted data
         $combinedData = base64_encode($iv . $encryptedData);
 
         return $combinedData;
     }
 
-    public function unformatData($data) { //Convert received data to the original format.
+    /**
+     * Decrypt and unformat the data using the specified encryption algorithm and the encryption key
+     * @param string $formattedData The encrypted and formatted data
+     * @return string The decrypted and unformatted data
+     */
+    public function unformatData($formattedData) {
+        $key = $this->encryptionKey;
+        $cipher = 'aes-256-cbc'; // Choose the encryption algorithm
+        $ivlen = openssl_cipher_iv_length($cipher);
 
+        // Decode the base64-encoded formatted data
+        $decodedData = base64_decode($formattedData);
+
+        // Extract the initialization vector and the encrypted data
+        $iv = substr($decodedData, 0, $ivlen);
+        $encryptedData = substr($decodedData, $ivlen);
+
+        // Decrypt the data using the key and the chosen cipher
+        $unformattedData = openssl_decrypt($encryptedData, $cipher, $key, 0, $iv);
+
+        return $unformattedData;
     }
+
     public function encryptData($data) { //Encrypt data for secure transmission.
 
     }
