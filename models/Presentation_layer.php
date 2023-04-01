5ae790f853deb54599349f9c98f9b99071531310
KBarna0315 <kskovacsb@gmail.com>
2023-04-01 02:50:11 +0200

    dev: Presentation_layer formatData openSSL encryption of the data

--- models/Presentation_layer.php
+++ models/Presentation_layer.php
@@ -4,9 +4,38 @@ namespace models;
 
 class Presentation_layer
 {
-    public function formatData($data) { // Convert data to a common format that both sender and receiver can understand.
+
+
+
+
+
+    /**
+     * Create a consistent encryption key that will be used for both encryption and decryption
+     * @return string
+     */
+    public function generateEncryptionKey() {
+        $key = openssl_random_pseudo_bytes(32);
+        return base64_encode($key);
 
     }
+    /**
+     * Format and encrypt the data using a specified encryption algorithm and an encryption key
+     * @param string $data The data to be encrypted
+     * @return string The encrypted and formatted data
+     */
+    public function formatData($data) {
+        $key = $this->generateEncryptionKey();
+        $cipher = 'aes-256-cbc'; // Choose the encryption algorithm
+        $ivlen = openssl_cipher_iv_length($cipher);
+        $iv = openssl_random_pseudo_bytes($ivlen); // Generate an initialization vector
+        // Encrypt the data using the key and the chosen cipher
+        $encryptedData = openssl_encrypt($data, $cipher, $key, 0, $iv);
+        // Combine the initialization vector and the encrypted data
+        $combinedData = base64_encode($iv . $encryptedData);
+
+        return $combinedData;
+    }
+
     public function unformatData($data) { //Convert received data to the original format.
 
     }
