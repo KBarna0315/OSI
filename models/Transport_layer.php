ac6847b1fa3480a4f6bdf7eb82012b0307478cd7
KBarna0315 <kskovacsb@gmail.com>
2023-04-06 06:33:02 +0200

    dev: Checksum calculation

--- models/Transport_layer.php
+++ models/Transport_layer.php
@@ -27,14 +27,47 @@ class Transport_layer
      */
    public function validateChecksum($payload, $receivedChecksum) {
         // Compute the checksum for the payload
-        $computedChecksum = crc32($payload);
+       $calculatedChecksum = $this->calculateChecksum($payload);
 
         // Compare the computed checksum with the received checksum
-        if ($computedChecksum === $receivedChecksum) {
+        if ($calculatedChecksum === $receivedChecksum) {
             return true;
         } else {
             return false;
         }
     }
+
+    /**
+     * Calculate the Internet Checksum for the given data.
+     * The Internet Checksum is a simple checksum algorithm used by IP and TCP
+     * to validate the integrity of the transmitted data.
+     *
+     * @param string $data The data for which the checksum should be calculated
+     * @return int The calculated checksum
+     */
+    public function calculateChecksum($data) {
+        $sum = 0;
+        $length = strlen($data);
+
+        // Iterate over the data in 2-byte (16-bit) chunks
+        for ($i = 0; $i < $length; $i += 2) {
+            // Combine the two bytes into a 16-bit word
+            $word = ord($data[$i]) << 8;
+            if ($i + 1 < $length) {
+                $word += ord($data[$i + 1]);
+            }
+
+            // Add the 16-bit word to the sum
+            $sum += $word;
+
+            // Handle any overflow by folding it back into the 16-bit range
+            $sum = ($sum & 0xffff) + ($sum >> 16);
+        }
+
+        // Invert the bits of the sum to create the final checksum
+        $sum = ~$sum & 0xffff;
+        return $sum;
+    }
+
 
 }
