fec798dee55de39d7c64f4fec2e43b627fbcfdc1
KBarna0315 <kskovacsb@gmail.com>
2023-04-06 07:21:53 +0200

    dev: routePacket with latency and packetLoss simulation

--- models/Network_layer.php
+++ models/Network_layer.php
@@ -4,8 +4,51 @@ namespace models;
 
 class Network_layer
 {
-    public function routePacket($data,$destination) { //Determine the best route for data transmission based on the destination IP address.
+    private float $congestionProbability = 0.1; // Example congestion probability
+    private float $linkFailureProbability = 0.05; // Example link failure probability
+    private float $randomLossProbability = 0.02; // Example random packet loss probability
+    /**
+     * Route the data packet through the network, simulating a real-world router
+     *
+     * @param array $dataPacket
+     * @return array
+     */
+    public function routePacket($dataPacket) {
+        // Simulate network latency by adding a delay
+        $this->simulateNetworkLatency();
 
+        // Simulate random packet loss
+        if ($this->simulatePacketLoss()) {
+            // In a real-world scenario, the router would detect the packet loss and initiate a retransmission.
+            // However, we won't be able to demonstrate retransmission in this simulation.
+            return null;
+        }
+
+        // Return the data packet unchanged, as we are not modifying it during routing
+        return $dataPacket;
+    }
+    /**
+     * Simulate network latency by adding a delay to the execution
+     */
+    private function simulateNetworkLatency() {
+        usleep(rand(10000, 100000)); // Simulate latency between 10ms and 100ms
+    }
+    /**
+     * Simulate packet loss in the network
+     *
+     * @return bool True if the packet is considered lost, False otherwise
+     */
+    public function simulatePacketLoss() {
+        // Simulate network congestion
+        $congestion = mt_rand(0, 100) / 100 < $this->congestionProbability;
+
+        // Simulate link failure
+        $linkFailure = mt_rand(0, 100) / 100 < $this->linkFailureProbability;
+
+        // Simulate random packet loss
+        $randomLoss = mt_rand(0, 100) / 100 < $this->randomLossProbability;
+
+        return $congestion || $linkFailure || $randomLoss;
     }
     public function forwardPacket($packet) { //Forward the packet to the next hop in the routing path.
 
