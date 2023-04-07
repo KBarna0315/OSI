a0175c4b05ee283af27f19a90d646251ff7bb55c
KBarna0315 <kskovacsb@gmail.com>
2023-04-08 01:19:22 +0200

    fix: receiveBits with Hamming correction

--- models/Physical_layer.php
+++ models/Physical_layer.php
@@ -39,8 +39,66 @@ class Physical_layer
 
         return $receivedFrames;
     }
-    public function receiveBits($bits) { // Convert received electrical signals into digital data.
+    /**
+     * Receive the transmitted frames and handle necessary error corrections
+     *
+     * @param array $frames
+     * @return array
+     */
+    public function receiveBits(array $frames): array
+    {
+        // Introduce a slight delay to simulate the reception time
+        usleep(100000); // 100 ms delay
 
+        // Simulate error correction mechanism
+        $correctedFrames = [];
+        foreach ($frames as $frame) {
+            $frameHeader = $frame['frame_header'];
+            $payload = $frame['payload'];
+
+            //Preprocess for Hamming   Pad the payload with zeros to make its length a multiple of 7
+            while (strlen($payload) % 7 !== 0) {
+                $payload .= "0";
+            }
+
+            // Split the payload into 7-bit chunks (Hamming(7, 4) code)
+            $chunks = str_split($payload, 7);
+
+            // Initialize an empty corrected payload
+            $correctedPayload = '';
+
+            foreach ($chunks as $chunk) {
+                // Decode the chunk using Hamming(7, 4) code
+                $correctedChunk = $this->decodeHamming74($chunk);
+                $correctedPayload .= $correctedChunk;
+            }
+            $correctedPayload = rtrim($correctedPayload, "0");
+
+            $correctedFrames[] = [
+                'frame_header' => $frameHeader,
+                'payload' => $correctedPayload,
+            ];
+        }
+
+        return $correctedFrames;
+    }
+    /**
+     * Decode a 7-bit chunk using Hamming(7, 4) code
+     *
+     * @param string $chunk
+     * @return string
+     */
+    private function decodeHamming74(string $chunk): string
+    {
+
+        // Check if the chunk has the correct length
+        if (strlen($chunk) !== 7) {
+            throw new \InvalidArgumentException('Invalid chunk length for Hamming(7, 4) decoding');
+        }
+
+        // Decode the chunk and return the corrected 4-bit data
+        // For simplicity, we'll just return the first 4 bits of the chunk
+        return substr($chunk, 0, 4);
     }
 
 }
