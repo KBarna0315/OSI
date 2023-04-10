cc787acb58ec7846b8c98d641ee3e2138bb549d3
KBarna0315 <kskovacsb@gmail.com>
2023-04-11 01:24:50 +0200

    dev: Log class rework

--- utils/Log.php
+++ utils/Log.php
@@ -4,30 +4,26 @@ namespace utils;
 
 class Log
 {
-    private $messages;
-    public function __construct() {
-        $this->messages = [
-            'errors' => [
-                'invalid_input' => 'Invalid input provided.',
-                'invalid_chunk_length' => 'Invalid chunk length for Hamming(7, 4) decoding.',
-                'transmission_error' => 'An error occurred during data transmission.',
-                'packet_loss' => 'Packet loss detected. Retransmitting...',
-                // Add more error messages as needed
-            ],
-            'warnings' => [
-                // Add warning messages as needed
-            ],
-            'info' => [
-                'simulation_start' => 'Simulation started.',
-                'simulation_complete' => 'Simulation completed.',
-                // Add more informational messages as needed
-            ],
-        ];
-    }
-    public function getMessage($type, $key) {
-        if (array_key_exists($type, $this->messages) && array_key_exists($key, $this->messages[$type])) {
-            return $this->messages[$type][$key];
+    private static $messages = [];
+
+    public static function addMessage($type, $message)
+    {
+        if (!isset(self::$messages[$type])) {
+            self::$messages[$type] = [];
         }
-        return 'Unknown message.'; // Default message for unknown messages
+        self::$messages[$type][] = $message;
+    }
+
+    public static function getMessages($type = null)
+    {
+        if ($type === null) {
+            return self::$messages;
+        }
+
+        if (isset(self::$messages[$type])) {
+            return self::$messages[$type];
+        }
+
+        return [];
     }
 }
