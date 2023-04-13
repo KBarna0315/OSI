5bf1ae21af956ee57e1d8389e6822af175aafebf
KBarna0315 <kskovacsb@gmail.com>
2023-04-13 10:29:43 +0200

    dev: createSession reworked only to return the id

--- models/Session_layer.php
+++ models/Session_layer.php
@@ -17,37 +17,29 @@ class Session_layer
 
     /**
      * Simulate creating a session between sender and receiver.
+     * @param int $timeout The session timeout in seconds (default 30 seconds)
      * @return string The unique session ID
-     * @throws \Exception If there's an error while creating the session
      */
-    public function createSession($timeout = 30): string
-    { //Establish a session with the destination node.
-        try {
-            // Generate a unique session ID
-            $this->sessionId = uniqid('session_', true);
+    public function createSession(int $timeout = 30): string
+    {
+        // Generate a unique session ID
+        $this->sessionId = uniqid('session_', true);
 
-            // Set the session status to 'established'
-            $this->sessionStatus = 'established';
+        // Set the session status to 'established'
+        $this->sessionStatus = 'established';
 
-            // Set the session timeout
-            $this->timeout = $timeout;
+        // Set the session timeout
+        $this->timeout = $timeout;
 
-            // Store additional session metadata
-            $this->sessionMetadata = [
-                'start_time' => time(),
-                'end_time' => time() + $timeout,
-            ];
+        // Store additional session metadata
+        $this->sessionMetadata = [
+            'start_time' => time(),
+            'end_time' => time() + $timeout,
+        ];
 
-            // Log the successful session creation
-            Log::addMessage('info', 'Session created successfully.');
-            return $this->sessionId;
-
-        } catch (\Exception $e) {
-            // Log the error and re-throw it
-            Log::addMessage('error', 'An error occurred while creating session: ' . $e->getMessage());
-
-            throw new \Exception("Error creating session: " . $e->getMessage());
-        }
+        // Log the successful session creation
+        Log::addMessage('info', 'Session created successfully.');
+        return $this->sessionId;
     }
     /**
      * Close the session and clear session-related metadata.
