544138bf5ab10c97756c8c9469c4c412bb321557
KBarna0315 <kskovacsb@gmail.com>
2023-04-17 23:34:10 +0200

    dev: routePacket rework

--- models/Network_layer.php
+++ models/Network_layer.php
@@ -9,27 +9,52 @@ class Network_layer
     private float $congestionProbability = 0.1; // Example congestion probability
     private float $linkFailureProbability = 0.05; // Example link failure probability
     private float $randomLossProbability = 0.02; // Example random packet loss probability
+    private $routerIpAddress;
+    private $routingTable;
+    public function __construct() {
+        $this->routerIpAddress = '192.168.1.1'; // Set the router IP address
+        $this->routingTable = [
+            '192.168.1.0/24' => '192.168.1.1',
+            '192.168.2.0/24' => '192.168.2.1',
+            '192.168.3.0/24' => '192.168.3.1',
+            '0.0.0.0/0' => '10.0.0.1', // Default route
+        ];
+    }
 
     /**
      * Route the data packet through the network, simulating a real-world router
-     *
      * @param array $dataPacket
      * @return array|null
      */
-    public function routePacket(array $dataPacket): ?array{
+    public function routePacket(array $dataPacket): ?array
+    {
         try {
             // Simulate network latency by adding a delay
             $this->simulateNetworkLatency();
 
             // Simulate random packet loss
             if ($this->simulatePacketLoss()) {
-                // In a real-world scenario, the router would detect the packet loss and initiate a retransmission.
-                // However, we won't be able to demonstrate retransmission in this simulation.
                 return null;
             }
 
-            // Return the data packet unchanged, as we are not modifying it during routing
-            Log::addMessage('info','Routing the data packet through the network.');
+            // Extract destination IP address from the data packet header
+            $destinationIpAddress = $dataPacket['header']['destination_ip'];
+
+            // Look up the appropriate route for the destination IP in the routing table
+            $nextHopIpAddress = $this->findNextHop($destinationIpAddress);
+
+            if ($nextHopIpAddress === null) {
+                throw new \Exception("No route found for destination IP: " . $destinationIpAddress);
+            }
+
+            // Update the data packet header with new source and destination IP addresses
+            $dataPacket['header']['source_ip'] = $this->routerIpAddress;
+            $dataPacket['header']['destination_ip'] = $nextHopIpAddress;
+
+            // Log the routing action
+            Log::addMessage('info', 'Routing the data packet through the network.');
+
+            // Return the repackaged data packet
             return $dataPacket;
         } catch (\Exception $e) {
             // Log the error
@@ -95,6 +120,27 @@ class Network_layer
                 'error' => 'Error: ' . $e->getMessage(),
             ];
         }
+    }
+    public function findNextHop(string $destinationIp): string {
+        // Find the matching route in the routing table
+        foreach ($this->routingTable as $route => $nextHop) {
+            // Check if the destination IP matches the route subnet
+            if ($this->ipMatchesSubnet($destinationIp, $route)) {
+                return $nextHop;
+            }
+        }
+
+        // No matching route found, return an empty string
+        return '';
+    }
+
+    private function ipMatchesSubnet(string $ip, string $subnet): bool {
+        // This method checks if the given IP address belongs to the specified subnet
+        // Implementing the actual IP-to-subnet matching logic is beyond the scope of this answer
+        // You can find a suitable implementation or library for this task
+
+        // For demonstration purposes, let's assume this function returns true if there's a match
+        return true;
     }
 
 
