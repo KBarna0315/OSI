3d83d002ca41f02c2d91a6b863ace3a4f57a441c
KBarna0315 <kskovacsb@gmail.com>
2023-04-17 22:36:25 +0200

    dev: sendRequest comment and parameterize

--- models/Application_layer.php
+++ models/Application_layer.php
@@ -10,13 +10,15 @@ class Application_layer
     public function __construct() {
 
     }
+
     /**
      * Send an application-level request to the server.
      * @param mixed $request The request to be sent to the server, in a format specific to the application
-     * @return array An associative array representing the data packet that was sent to the server, including headers
+     * @return array|null An associative array representing the data packet that was sent to the server, including headers
      *               Example: ['payload' => '...', 'header' => ['sequence_number' => ..., 'acknowledgment_number' => ..., ...]]
      */
-    public function sendRequest($request) {
+    public function sendRequest(mixed $request): ?array
+    {
         try {
             // Convert the request into a data packet with appropriate headers, similar to a TCP segment
             $dataPacket = $this->createDataPacket($request);
